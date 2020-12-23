119ff4b8c87bd0f721858c2e1f010f04bdddde9d
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2020-12-23 15:12:35 -0300

    GUI HEBB PERCEPTRON Beta

--- 5_gui_hebb_perceptron/hebb.py
+++ 5_gui_hebb_perceptron/hebb.py
@@ -7,14 +7,14 @@ def __init__(self, values_):
     self.train = values_
 def step_zero(self):
     self.weights = []
-for i in range(len(self.train[0][0])):
-    self.weights.append(0)
+    for i in range(len(self.train[0][0])):
+        self.weights.append(0)
 
 def step_n(self):
     self.step_zero()
-for ct in self.train:
-    put = list(ct[0])
+    for ct in self.train:
+        put = list(ct[0])
     out = ct[1]
     for i in range(len(self.weights)):
         self.weightsh[i] = self.weights[i] + put[i] * out
-return self.weights
+    return self.weights
