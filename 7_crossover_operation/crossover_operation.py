fec8a09bd50d2e84e98171f3ae2c3626059185ce
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2020-12-23 11:12:14 -0300

    cruzamento

--- 7_crossover_operation/crossover_operation.py
+++ 7_crossover_operation/crossover_operation.py
@@ -63,29 +63,29 @@ def on_method_combobox_current_text_changed():
 def pmx_crossover():
     cuts = 2
     trades = 1
-    papa = daddy_line_edit.text()
-    mama = mammy_line_edit.text()
-    parts = random.sample(range(1, len(papa)), cuts)
+    papi = daddy_line_edit.text()
+    mami = mammy_line_edit.text()
+    parts = random.sample(range(1, len(papi)), cuts)
     parts.sort()
     choose = random.sample(range(0, len(parts)+1), trades)
     filhao_1 = []
     for i in range (len(parts)+1):
         filhao_1.append(0)
         if i == 0:
-            filhao_1[i] = papa[:parts[0]]
+            filhao_1[i] = papi[:parts[0]]
         elif i == len(parts):
-            filhao_1[i] = papa[parts[i-1]:]
+            filhao_1[i] = papi[parts[i-1]:]
         else:
-            filhao_1[i] = papa[parts[i-1]:parts[i]]
+            filhao_1[i] = papi[parts[i-1]:parts[i]]
     filhao_2 = []
     for i in range(len(parts) + 1):
         filhao_2.append(0)
         if i == 0:
-            filhao_2[i] = mama[:parts[0]]
+            filhao_2[i] = mami[:parts[0]]
         elif i == len(parts):
-            filhao_2[i] = mama[parts[i - 1]:]
+            filhao_2[i] = mami[parts[i - 1]:]
         else:
-            filhao_2[i] = mama[parts[i - 1]:parts[i]]
+            filhao_2[i] = mami[parts[i - 1]:parts[i]]
     filhao_x = filhao_1.copy()
     filhao_1_muda_1 = ""
     filhao_1_muda_2 = ""
@@ -121,29 +121,29 @@ def pmx_crossover():
     filhao2_label_3.setText(filhao_2[2])
 def simple_cut_crossover(cuts):
     trades = 1
-    papa = daddy_line_edit.text()
-    mama = mammy_line_edit.text()
-    parts = random.sample(range(1, len(papa)), cuts)
+    papi = daddy_line_edit.text()
+    mami = mammy_line_edit.text()
+    parts = random.sample(range(1, len(papi)), cuts)
     parts.sort()
     choose = random.sample(range(0, len(parts)+1), trades)
     filhao_1 = []
     for i in range (len(parts)+1):
         filhao_1.append(0)
         if i == 0:
-            filhao_1[i] = papa[:parts[0]]
+            filhao_1[i] = papi[:parts[0]]
         elif i == len(parts):
-            filhao_1[i] = papa[parts[i-1]:]
+            filhao_1[i] = papi[parts[i-1]:]
         else:
-            filhao_1[i] = papa[parts[i-1]:parts[i]]
+            filhao_1[i] = papi[parts[i-1]:parts[i]]
     filhao_2 = []
     for i in range(len(parts) + 1):
         filhao_2.append(0)
         if i == 0:
-            filhao_2[i] = mama[:parts[0]]
+            filhao_2[i] = mami[:parts[0]]
         elif i == len(parts):
-            filhao_2[i] = mama[parts[i - 1]:]
+            filhao_2[i] = mami[parts[i - 1]:]
         else:
-            filhao_2[i] = mama[parts[i - 1]:parts[i]]
+            filhao_2[i] = mami[parts[i - 1]:parts[i]]
     filhao_x = filhao_1.copy()
     for i in range(len(choose)):
         filhao_1[choose[i]] = filhao_2[choose[i]]
